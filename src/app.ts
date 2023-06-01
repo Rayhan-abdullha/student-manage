fdf972bc3e0e17feac96562890dc26b179902d84
Rayhan-abdullha <captainrayhan215@gmail.com>
2023-06-01 10:00:45 +0600

    testing

--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,11 @@ import cors from 'cors'
 const app: Application = express()
 app.use([express.json(), cors(), urlencoded({ extended: true })])
 
-app.get('/', (req:Request, res: Response) => {
+app.get('/', (req:Request, res: any) => {
   res.send('Hello server..')
 })
-
+app.get('/', (req:Request, res: Response) => {
+    res.send('Hello server..')
+  })
+  
 export default app
